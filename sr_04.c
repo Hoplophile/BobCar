6969724a7f6826f6a1e1c837a87e7a47a67eb085
Cztery <kurkowski.mrcn@gmail.com>
2020-07-02 00:34:46 +0200

    added comments to proximity sensor functions

--- sr_04.c
+++ sr_04.c
@@ -7,6 +7,19 @@
 #include <stdlib.h>
 #include <util/delay.h>
 
+/* sensor_new
+ * Creates and initializes a new structure representing a particular HC-SR04 proximity sensor
+ * Params:
+ *   measure_iter: number of measurements to execute for an averaged distance
+ *   trig_DDRX: trig port direction register
+ *   trig_DDXY: trig port direction bit mask
+ *   trig_PORTX: trig port register
+ *   trig_PORTXY: trig port bit mask
+ *   echo_DDRX: echo port direction register
+ *   echo_DDXY: echo port direction bit mask
+ *   echo_PINX: echo pin register
+ *   echo_PINXY: echo pin bit mask
+ */
 sr_04 *sensor_new(uint8_t measure_iter,
                   volatile uint8_t *trig_DDRX,
                   uint8_t trig_DDXY,
@@ -49,6 +62,11 @@ void set_trig_low(sr_04 *sensor) { *(sensor->_trig_PORTX) &= ~(1 << sensor->_tri
 
 bool read_echo(sr_04 *sensor) { return (bool)(*(sensor->_echo_PINX) & (1 << sensor->_echo_PINXY)); }
 
+/* measure_duration
+ * triggers the distance measurement using specified sensor and stores the measured echo pulse duration in sensor struct
+ * Params:
+ *   sensor: pointer to a structure describing the sensor
+ */
 void measure_duration(sr_04 *sensor) {
   set_trig_low(sensor);
   _delay_us(2);
@@ -71,10 +89,20 @@ void measure_duration(sr_04 *sensor) {
   TCCR1B &= ~((1 << CS12)|(1 << CS11)|(1 << CS10));
 }
 
+/* calculate_distance_mm
+ * returns the distance in millimeters
+ * Params:
+ *   duration: sensor echo pulse duration measured using timer with prescaler 64
+ */
 int calculate_distance_mm(uint16_t duration) {
   return (unsigned long)((double)duration * 0.686);
 }
 
+/* get_dist_cm
+ * triggers distance measurement, update duration and distance latest values in specified structure and return the distance in millimeters
+ * Params:
+ *   sensor: pointer to a structure describing the sensor
+ */
 int get_dist_mm(sr_04 *sensor) {
   measure_duration(sensor);
   sensor->_last_distance_mm = calculate_distance_mm(sensor->_last_duration);
